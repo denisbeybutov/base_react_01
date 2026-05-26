fbe30b6f09130ad262a000d472ef888a96e4f21c
denisbeybutov <beybutov.denis@mail.ru>
2026-05-26 06:49:33 +0300

    четвертый пукнт готов

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,21 +5,25 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import People from './people/people';
 import UserProfile from './userProfile/userProfile';
+import { USERS } from './users'
 
-const user = {
-  name: 'Ivan',
-  email: 'ivan@google.com',
-  avatarUrl: 'https://i.pinimg.com/736x/63/5a/57/635a57abc0779e5529df046eb08004f2.jpg',
-  bio: "Hello. My name is Ivan. I'm a programmist"
-}
+
 
 function App() {
+
+  let outputUsers = USERS.map((person) => (
+     <UserProfile key={person.id} user={person}/>
+   ) )
+
 
   return (
     <>      
       <h1>My project</h1>     
       <People number={1}/>
-      <UserProfile user={user}/>
+      <div className='user__list'>
+        {outputUsers}
+      </div>
+      
       
     </>
   )
